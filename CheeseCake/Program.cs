ddb09254f80a1d0dcfd386b48612bd299d4e8d81
kyubuns <kyubuns@gmail.com>
2016-06-07 16:46:30 +0900

    first

---
+++ CheeseCake/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Linq;
+using ICSharpCode.NRefactory.CSharp;
+
+namespace CheeseCake
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      if (args.Length != 2) {
+        Console.WriteLine ("Usage: CheeseCake FileName OptionFileName");
+        return;
+      }
+
+      var sourceFileName = args [0];
+      var optionFileName = args [1];
+
+      var policy = LoadFormattingProfile (optionFileName);
+      var options = LoadTextEditorProfile (optionFileName);
+      var formatter = new CSharpFormatter (policy, options);
+
+      var text = File.ReadAllText(sourceFileName);
+      File.WriteAllText (sourceFileName, formatter.Format (text), Encoding.UTF8);
+    }
+
+    public static CSharpFormattingOptions LoadFormattingProfile (string selectedFile)
+    {
+      using (var stream = System.IO.File.OpenRead (selectedFile)) {
+        return LoadFormattingProfile (stream);
+      }
+    }
+
+    public static CSharpFormattingOptions LoadFormattingProfile (System.IO.Stream input)
+    {
+      CSharpFormattingOptions result = FormattingOptionsFactory.CreateMono();
+      result.Name = "noname";
+      using (XmlTextReader reader = new XmlTextReader (input)) {
+        reader.ReadToFollowing ("Options");
+        reader.ReadToDescendant ("FormattingProfile");
+        while (reader.Read()) {
+          if (reader.NodeType == XmlNodeType.Element) {
+            if (reader.LocalName == "Property") {
+              var info = typeof(CSharpFormattingOptions).GetProperty (reader.GetAttribute ("name"));
+              string valString = reader.GetAttribute ("value");
+              object value;
+              if (info.PropertyType == typeof(bool)) {
+                value = Boolean.Parse (valString);
+              } else if (info.PropertyType == typeof(int)) {
+                value = Int32.Parse (valString);
+              } else {
+                value = Enum.Parse (info.PropertyType, valString);
+              }
+              info.SetValue (result, value, null);
+            } else if (reader.LocalName == "FormattingProfile") {
+              result.Name = reader.GetAttribute ("name");
+            }
+          } else if (reader.NodeType == XmlNodeType.EndElement && reader.LocalName == "FormattingProfile") {
+            return result;
+          }
+        }
+      }
+      return result;
+    }
+
+    public static TextEditorOptions LoadTextEditorProfile (string selectedFile)
+    {
+      using (var stream = System.IO.File.OpenRead (selectedFile)) {
+        return LoadTextEditorProfile (stream);
+      }
+    }
+
+    public static TextEditorOptions LoadTextEditorProfile (System.IO.Stream input)
+    {
+      TextEditorOptions result = new TextEditorOptions ();
+      using (XmlTextReader reader = new XmlTextReader (input)) {
+        reader.ReadToFollowing ("Options");
+        reader.ReadToDescendant ("TextEditorProfile");
+        while (reader.Read()) {
+          if (reader.NodeType == XmlNodeType.Element) {
+            if (reader.LocalName == "Property") {
+              var info = typeof(TextEditorOptions).GetProperty (reader.GetAttribute ("name"));
+              string valString = reader.GetAttribute ("value");
+              object value;
+              if (info.PropertyType == typeof(bool)) {
+                value = Boolean.Parse (valString);
+              } else if (info.PropertyType == typeof(int)) {
+                value = Int32.Parse (valString);
+              } else if (info.PropertyType == typeof(string)) {
+                value = valString;
+              } else {
+                value = Enum.Parse (info.PropertyType, valString);
+              }
+              info.SetValue (result, value, null);
+            }
+          } else if (reader.NodeType == XmlNodeType.EndElement && reader.LocalName == "TextEditorProfile") {
+            return result;
+          }
+        }
+      }
+      return result;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CheeseCake
 C#のコードを綺麗にするやつ
+
+# 使い方
+mono CheeseCake.exe Target.cs Option.xml
+
+# オプションの設定方法
+sample.optionsを参考にしてください。
+内容は↓を参考に。
+https://github.com/icsharpcode/NRefactory/blob/master/ICSharpCode.NRefactory.CSharp/Formatter/CSharpFormattingOptions.cs
